0bd9591508573257c27a4b4b7e66487b08bafb3a
Gobi200 <126998747+Gobi200@users.noreply.github.com>
2023-03-05 12:34:14 +0530

    Update questions.sql

--- SQL Deep Dive/Operator precendence/questions.sql
+++ SQL Deep Dive/Operator precendence/questions.sql
@@ -7,6 +7,8 @@
 * Include people that are 50
 * that are from either Japan or Australia
 */
+select firstname,age,income from customers where (age<30 or age>=50) and income>50000 and (country='Japan' or country='Russia');
+
 
 /*
 * DB: Store
@@ -14,3 +16,4 @@
 * Question: 
 * What was our total sales in June of 2004 for orders over 100 dollars?
 */
+select sum(totalamount) from orders where (orderdate>='2004-06-01' and orderdate<='2004-06-30') and totalamount>100;
