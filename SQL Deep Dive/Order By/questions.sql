de60a57bed58968c3aa5631a79684e5303912c2b
Gobi200 <126998747+Gobi200@users.noreply.github.com>
2023-03-15 13:21:51 +0530

    Update questions.sql

--- SQL Deep Dive/Order By/questions.sql
+++ SQL Deep Dive/Order By/questions.sql
@@ -3,17 +3,18 @@
 * Table: employees
 * Question: Sort employees by first name ascending and last name descending
 */
-
+select * from employees order by first_name asc, last_name desc;
 
 /*
 * DB: Employees
 * Table: employees
 * Question: Sort employees by age
 */
-
+select * from employees order by birth_date;
 
 /*
 * DB: Employees
 * Table: employees
 * Question: Sort employees who's name starts with a "k" by hire_date
 */
+select * from employees where first_name ilike 'k%' order by  hire_date;
