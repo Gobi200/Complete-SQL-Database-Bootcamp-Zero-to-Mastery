696bccac748622c016467c56b0fe3034505bfd38
Gobi200 <126998747+Gobi200@users.noreply.github.com>
2023-03-18 20:30:14 +0530

    Update questions.sql

--- SQL Deep Dive/Group By/questions.sql
+++ SQL Deep Dive/Group By/questions.sql
@@ -4,22 +4,25 @@
 *  Table: Employees
 */
 
-SELECT e.emp_no
-FROM employees as e
+select hire_date,count(emp_no) from employees
+group by hire_date;
 
 /*
 *   Show me all the employees, hired after 1991 and count the amount of positions they've had
 *  Database: Employees
 */
 
-SELECT e.emp_no
-FROM employees as e
+select a.emp_no,count(b.title) from employees as a 
+inner join titles as b using(emp_no)
+where extract(year from a.hire_date) > 1991
+group by emp_no 
+order by emp_no;
 
 
 /*
 *  Show me all the employees that work in the department development and the from and to date.
 *  Database: Employees
 */
-
-SELECT e.emp_no
-FROM employees as e
+select emp_no,from_date,to_date from dept_emp
+where dept_no ='d005'
+order by emp_no;
